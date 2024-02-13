e14382045377fafabab372a8d9fb51452424b9ca
Luis <luis.martinezlopez@unicolombo.edu.co>
2024-02-12 23:14:51 -0500

    Terminado

---
+++ Ejercicio3.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class Ejercicio3 {
+    public static void main(String[] args) {
+        Scanner entrada = new Scanner(System.in);
+        System.out.print("Digite la base de un triangulo: ");
+        int b=entrada.nextInt();
+        System.out.print("Digite la altura de un triangulo: ");
+        int h=entrada.nextInt();
+        entrada.close();
+        int area = (b*h)/2;
+        System.out.println("Area = "+area);
+    }
+}
