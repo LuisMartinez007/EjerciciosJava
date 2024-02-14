b32ea8fbd4f65982de8f5bd848c6c06e2c68e870
Luis <luis.martinezlopez@unicolombo.edu.co>
2024-02-13 19:29:47 -0500

    Terminado

---
+++ Ejercicio4.java
@@ -0,0 +1,11 @@
+import java.util.*;
+
+public class Ejercicio4 {
+    public static void main(String[] args) {
+        Scanner entrada =new Scanner(System.in);
+        System.out.print("Digite el radio de un circulo: ");
+        float r= entrada.nextFloat();
+        entrada.close();
+        double area= Math.PI *(r*r);
+    }
+}
