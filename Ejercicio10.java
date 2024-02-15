03717efc029d61b2b64369b124daf73d3263c59b
Luis <luis.martinezlopez@unicolombo.edu.co>
2024-02-15 16:35:54 -0500

    Base de un triangulo

---
+++ Ejercicio10.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class Ejercicio10 {
+    public static void main(String[] args) {
+        Scanner entrada = new Scanner(System.in);
+        System.out.println("***************************");
+        System.out.println("HALLAR BASE DE UN TRIANGULO");
+        System.out.println("***************************");
+        System.out.print("Digite la base de un triangulo: ");
+        int b=entrada.nextInt();
+        System.out.print("Digite la altura de un triangulo: ");
+        int h=entrada.nextInt();
+        entrada.close();
+        int area = (b*h)/2;
+        System.out.println("Area = "+area);
+    }
+}
