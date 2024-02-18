7f6d19b8ebc48bcdad89e119510be402d51d1036
Luis <luis.martinezlopez@unicolombo.edu.co>
2024-02-18 14:02:00 -0500

    Area de un triangulo

--- Ejercicio10.java
+++ Ejercicio10.java
@@ -4,7 +4,7 @@ public class Ejercicio10 {
     public static void main(String[] args) {
         Scanner entrada = new Scanner(System.in);
         System.out.println("***************************");
-        System.out.println("HALLAR BASE DE UN TRIANGULO");
+        System.out.println("HALLAR AREA DE UN TRIANGULO");
         System.out.println("***************************");
         System.out.print("Digite la base de un triangulo: ");
         int b=entrada.nextInt();
