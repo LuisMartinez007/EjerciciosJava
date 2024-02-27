a78c4c9f1d5788c811ea10aae0b43cd38a58dadc
Luis <luis.martinezlopez@unicolombo.edu.co>
2024-02-26 21:52:17 -0500

    Conversion km a c:; cd 'c:\Users\Usuario\Desktop\Luis Martinez\Java\E.J 2 sem'; & 'C:\Program Files\Java\jdk-21\bin\java.exe' '--enable-preview' '-XX:+ShowCodeDetailsInExceptionMessages' '-cp' 'C:\Users\Usuario\AppData\Roaming\Code\User\workspaceStorage\fc6a3f5394a90247f031d263a7066fa7\redhat.java\jdt_ws\E.J 2 sem_9fd693c6\bin' 'Ejercicio30'

---
+++ Ejercicio30.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class Ejercicio30 {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        System.out.print("Digite una cantidad de kilometros: ");
+        float km = in.nextFloat();
+        in.close();
+        float cm = km*1000;
+        System.out.println(km+" km = "+ cm+" cm");
+    }
+}
