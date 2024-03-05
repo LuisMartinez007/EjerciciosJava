fae56d835570b58b76b492f591f6818fa8024ae9
Luis <luis.martinezlopez@unicolombo.edu.co>
2024-03-05 14:18:37 -0500

    Area de un rectangulo en m,cm y mm cuadrados

---
+++ Ejercicio32.java
@@ -0,0 +1,20 @@
+import java.util.*;
+public class Ejercicio32 {
+        public static void main(String[] args) {
+            Scanner entrada = new Scanner(System.in);
+            System.out.println("****************************");
+            System.out.println("HALLAR AREA DE UN RECTANGULO");
+            System.out.println("****************************");
+            System.out.print("Digite la base del rectangulo en metros: ");
+            double n1= entrada.nextDouble();
+            System.out.print("Digite la altura del rectangulo en metros: ");
+            double n2= entrada.nextDouble();
+            entrada.close();
+            double rM = n1*n2;
+            System.out.print("Area = "+rM+" M^2");
+            double rCM = rM*10000;
+            System.out.println("Area = "+rCM+" CM^2");
+            double rMM = rCM*100;
+            System.out.println("Area = "+rMM+" MM^2");
+        }
+}
