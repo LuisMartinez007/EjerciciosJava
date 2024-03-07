89abdeef3c6ab3fef3d47dfd3a8a95896af4083a
LuisMartinez007 <159143417+LuisMartinez007@users.noreply.github.com>
2024-03-07 07:57:37 -0500

    Estacionameinto 2

---
+++ Ejercicio37.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class Ejercicio37 {
+    public static void main(String args[]){
+  Scanner in = new Scanner(System.in);
+  System.out.print("Digite las horas que estuvo estacionado: ");
+  int hrs = in.nextInt();
+  System.out.print("Digite los minutos que estuvo estacionado: ");
+  double min = in.nextDouble();
+  in.close();
+        if(min<=10){
+            hrs = hrs*15;
+            System.out.println("\nDebe pagar: "+(hrs)+"$");
+        } else{
+            hrs++;
+            hrs = hrs*15;
+            System.out.println("\nDebe pagar: "+(hrs)+"$");
+        }
+    }
+}
