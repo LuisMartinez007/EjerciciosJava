865d3394d520fe4e5cf7a56c19f66d16eff686fe
Luis <luis.martinezlopez@unicolombo.edu.co>
2024-03-06 22:05:03 -0500

    Calcula salario de un profesor

---
+++ Ejercicio35.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class Ejercicio35 {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        System.out.print("Digite la tarifa por hora: ");
+        int tarifa = in.nextInt();
+        System.out.print("Digite las horas trabajadas: ");
+        int hrs = in.nextInt();
+        in.close();
+        if(hrs>40){
+            int hrsExtra = hrs -40;
+            int tarifaAumentada = tarifa +(tarifa/2);
+            System.out.println("Salario: "+((40*tarifa)+(hrsExtra*tarifaAumentada))+"$");
+        } else{
+            System.out.println("Salario: "+(tarifa*hrs)+"$");
+        }
+    }
+}
