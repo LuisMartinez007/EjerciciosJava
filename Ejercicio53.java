93cb678d2af04c100988f1f70c05512390e924bb
Luis <luis.martinezlopez@unicolombo.edu.co>
2024-03-10 00:02:26 -0500

    Area de un cuadrado

---
+++ Ejercicio53.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class Ejercicio53 {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        System.out.print("Digite el lado de un cuadrado: ");
+        double lado = in.nextDouble();
+        in.close();
+        double area = lado * lado;
+        System.out.println("El área del cuadrado es: " + area);
+    }
+}
