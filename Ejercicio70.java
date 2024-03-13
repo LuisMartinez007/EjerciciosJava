924df3b5cf83ddc7fe5357d31782a4c6cea6c886
Luis <luis.martinezlopez@unicolombo.edu.co>
2024-03-12 19:53:35 -0500

    Calcula el resultado de la siguiente expresión: 1-2+3-4+5-6...n

---
+++ Ejercicio70.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class Ejercicio70 {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        System.out.print("Digite un numero: ");
+        int n = in.nextInt();
+        in.close();
+        int resultado =0;
+        for(int i=1; i<=n ;i++){
+            if(i%2==0){
+                resultado-=i;
+            }else{
+                resultado+=i;
+            }
+        }
+        System.out.println("Resultado: "+resultado);
+    }
+}
