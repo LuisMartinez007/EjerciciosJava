0b10fe2080a897dd18bf9987f29b7c4ba8c938a4
Luis <luis.martinezlopez@unicolombo.edu.co>
2024-03-12 20:01:38 -0500

    Suma desde 21 hasta 29

--- Ejercicio69.java
+++ Ejercicio69.java
@@ -1,19 +1,9 @@
-import java.util.Scanner;
-
 public class Ejercicio69 {
     public static void main(String[] args) {
-        Scanner in = new Scanner(System.in);
-        System.out.print("Digite un numero: ");
-        int n = in.nextInt();
-        in.close();
-        int resultado =0;
-        for(int i=1; i<=n ;i++){
-            if(i%2==0){
-                resultado-=i;
-            }else{
-                resultado+=i;
-            }
+        int resultado=0;
+        for(int i=21;i<30;i++){
+            resultado+=i;
         }
-        System.out.println("Resultado: "+resultado);
+        System.out.println("La suma desde 21 hasta 29 es: "+resultado);
     }
 }
