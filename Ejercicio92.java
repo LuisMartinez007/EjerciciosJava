01a1a3f83d3ee2205d65b14f980fe0f9a7849767
Luis <luis.martinezlopez@unicolombo.edu.co>
2024-03-17 13:24:46 -0500

    Producto escalar de dos vectores

---
+++ Ejercicio92.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class Ejercicio92 {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        System.out.print("Ingrese el tamaño de los vectores: ");
+        int n = in.nextInt();
+        int[] vector1 = new int[n];
+        System.out.println("Ingrese los elementos del primer vector:");
+        for (int i = 0; i < n; i++) {
+            vector1[i] = in.nextInt();
+        }
+        int[] vector2 = new int[n];
+        System.out.println("Ingrese los elementos del segundo vector:");
+        for (int i = 0; i < n; i++) {
+            vector2[i] = in.nextInt();
+        }
+        in.close();
+        int productoEscalar = 0;
+        for (int i = 0; i < n; i++) {
+            productoEscalar += vector1[i] * vector2[i];
+        }
+        System.out.println("El producto escalar de los dos vectores es: " + productoEscalar);
+    }
+}
