48237db5d7a4ae897a07b35cc6dccff099903dab
Luis <luis.martinezlopez@unicolombo.edu.co>
2024-04-11 14:16:37 -0500

    Ejercicio corregido

--- Ejercicio115.java
+++ Ejercicio115.java
@@ -1,10 +1,11 @@
 public class Ejercicio115 {
     public static void main(String[] args) {
-        System.out.println("Numeros pares entre 1 y 1000");
+        int suma=0;
         for(int i=1;i<=1000;i++){
             if(i%2==0){
-                System.out.print(i+" ");
+                suma+=i;
             }
         }
+        System.out.println("La suma de numeros pares comprendidos entre 1 y 1000\nSuma: "+suma);
     }
 }
