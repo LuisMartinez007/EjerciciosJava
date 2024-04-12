6da029753da1ba055445b1a905d6875ddfb13340
Luis <luis.martinezlopez@unicolombo.edu.co>
2024-04-11 23:33:38 -0500

    Multiplos de 3 desde el 1 hasta 1000

---
+++ Ejercicio124.java
@@ -0,0 +1,10 @@
+public class Ejercicio124 {
+    public static void main(String[] args) {
+        System.out.println("Multiplos de 3 desde 1 hasta 3000");
+        for(int i=1;i<=3000;i++){
+            if(i%3==0){
+                System.out.print(i+" ");
+            }
+        }
+    }
+}
