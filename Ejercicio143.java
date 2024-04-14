db53c87a936c14bb5fad522e76adb409db476fb4
Luis <luis.martinezlopez@unicolombo.edu.co>
2024-04-13 22:06:28 -0500

    Suma de números pares entre 1 y 100

--- Ejercicio143.java
+++ Ejercicio143.java
@@ -1,9 +1,11 @@
 public class Ejercicio143 {
     public static void main(String[] args) {
+        int suma=0;
         for(int i=1;i<=100;i++){
             if(i%2==0){
-                System.out.print(i+" ");
+                suma+=i;
             }
         }
+        System.out.println("Suma de los números pares entre 1 y 100: "+suma);
     }
 }
